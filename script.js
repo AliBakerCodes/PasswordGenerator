c615c7e281ae6a51687af247872979a26abe1f2a
AliBaker <alibakerconsulting@gmailcom>
2022-03-16 22:45:19 -0400

    Built out main function to accept input, validate, generate, and display

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ var passwordInpt = {
   validLength: false,
   validType:false,
 };
+var allowLower="";
+var allowUpper="";
+var allowNumeric="";
+var allowSpecial="";
+
 var validChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","^","_","`","{","|","}","~"]
+
 
 
 // Write password to the #password input
 function writePassword() {
+  //Input prompts
+  prompt("Choose Password Length (8-128)", passwordInpt.length);
+  prompt("Lower Case Allowed? (Y/N)",allowLower);
+   //update the correct boolean on Y
+  if (allowLower==="Y"){
+    passwordInpt.lower=true;
+  }
+  prompt("Upper Case Allowed? (Y/N)",allowUpper);
+  if (allowUpper==="Y"){
+    passwordInpt.upper=true;
+  }
+  prompt("Numeric Case Allowed? (Y/N)",allowNumeric);
+  if (allowNumeric==="Y"){
+    passwordInpt.numeric=true;
+  }
+  prompt("Special Case Allowed? (Y/N)",allowSpecial);
+  if (allowSpecial==="Y"){
+    passwordInpt.special=true;
+  }
+//Validate input
+
+  passwordInpt=validateInput(passwordInpt)
+//Generate Password
   var password = generatePassword();
+//Output Password
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -24,14 +54,14 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function validateInput(passwordInpt){
-  if (passwordInpt.length<8 || passwordInpt.length >128){
-      passwordInpt.validLength=false;
+function validateInput(password){
+  if (password.length<8 || password.length >128){
+      password.validLength=false;
     }
-    if (passwordInpt.lower ===false ||passwordInpt.upper===false ||passwordInpt.numeric===false||passwordInpt.special===false){
-      passwordInpt.validType=false;
+    if (password.lower ===false ||password.upper===false ||password.numeric===false||password.special===false){
+      password.validType=false;
     }
-  return passwordInpt;
+  return password;
 };
 
 function randNum() {
@@ -58,8 +88,6 @@ function containsLower(str) {
   const lower = abcdefghijklmnopqrstuvwxyz;
   return lower.test(str);
 };
-
-
 
 function generatePassword(length, lower, upper, numeric, special) {
   var generatedPassword="";
