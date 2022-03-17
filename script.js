f39b08265241f97b64587aa43d0ecc46c8b9ab1c
AliBaker <alibakerconsulting@gmailcom>
2022-03-17 04:44:52 -0400

    Comments and removed console logs for debugging

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//Validates password inputs. Throw an error if password number out of 8-128 range or at least 1 character type not picked"
 function validateInput(password) {
   if (password.len >= 8 && password.len <= 128) {
     password.validLength = true;
@@ -70,43 +70,42 @@ function validateInput(password) {
     alert("You must select at least one valid type");
   }
 }
-
+// Randomly select a number between 0 and the number of allowed characters
 function randNum() {
   var index = Math.floor(Math.random() * validChars.length);
   return index;
 }
-
+//Tests for special characters
 function containsSpecialChars(str) {
   var regex = /[ !\"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~]/;
   return regex.test(str);
 }
-
+//Tests for numbers
 function containsNumbers(str) {
   const regex = /^[0-9]+$/;
   return regex.test(str);
 }
-
+//Tests for Upper Case
 function containsUpper(str) {
   const regex = /[A-Z]/;
   return regex.test(str);
 }
-
+//Tests for Lower Case
 function containsLower(str) {
   const regex = /[a-z]/;
   return regex.test(str);
 }
-
+//Generate the password
 function generatePassword(length, lower, upper, numeric, special) {
-  console.log("Generating Password");
-  console.log(lower);
+
   var generated = "";
   var tempchar = "";
-  var escapeChar = /[\'\"\[\]]/;
+// Generate random number. Pick from array of allowed characters. 
+// Test character to determine type and if user selected type.
+// If both match write to output string 
   do {
     tempchar = validChars[randNum()];
-    console.log("tempch is: " + tempchar);
-    console.log("lc " + (lower && containsLower(tempchar)));
-    if (lower && containsLower(tempchar)) {
+     if (lower && containsLower(tempchar)) {
       generated = generated + tempchar;
     } else if (upper && containsUpper(tempchar)) {
       generated = generated + tempchar;
@@ -116,35 +115,28 @@ function generatePassword(length, lower, upper, numeric, special) {
       generated = generated + tempchar;
     }
   } while (generated.length < length);
-  console.log(generated);
   return generated;
 }
-
+//Get user inputs for password length and character type using prompts
+//Sanitize inputs to uppercase
 function getUserInput() {
   //Input prompts
   passwordInpt.len = prompt("Choose Password Length (8-128)");
-  console.log(passwordInpt.len);
   allowLower = prompt("Lower Case Allowed? (Y/N)");
-  console.log(allowLower);
-  console.log(allowLower.toUpperCase());
   //update the correct boolean on Y
   if (allowLower.toUpperCase() == "Y") {
     passwordInpt.lower = true;
   }
   allowUpper = prompt("Upper Case Allowed? (Y/N)");
-  console.log(allowUpper);
   if (allowUpper.toUpperCase() == "Y") {
     passwordInpt.upper = true;
   }
   allowNumeric = prompt("Numeric Case Allowed? (Y/N)");
-  console.log(allowNumeric);
   if (allowNumeric.toUpperCase() == "Y") {
     passwordInpt.numeric = true;
   }
   allowSpecial = prompt("Special Case Allowed? (Y/N)");
-  console.log(allowSpecial);
   if (allowSpecial.toUpperCase() == "Y") {
     passwordInpt.special = true;
   }
-  console.log(passwordInpt);
 }
